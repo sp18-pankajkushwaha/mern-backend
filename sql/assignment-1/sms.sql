821b2d6393f1eb8ebfce59a5be40135348f581f7
Pankaj <pankaj.kushwaha@sparkeighteen.com>
2025-08-01 20:40:08 +0530

    Completed all queries

--- sql/assignment-1/sms.sql
+++ sql/assignment-1/sms.sql
@@ -1,36 +1,43 @@
-create database "SchoolManagementSystem"
+--Creating database
+CREATE DATABASE "SchoolManagementSystem"
 
-create table students(
-id serial primary key,
-name varchar(50) not null,
-age int check (age>0),
-gender varchar(20),
-email varchar(50) UNIQUE not null,
-phone varchar(15)
+--Creating students table
+CREATE TABLE students(
+id SERIAL PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+age INT CHECK (age>0),
+gender VARCHAR(20),
+email VARCHAR(50) UNIQUE NOT NULL,
+phone VARCHAR(15)
 )
 
-create table courses(
-id serial primary key,
-course_name varchar(30) not null,
-duration interval,
-fee decimal(10,2) 
+--Creating courses table
+CREATE TABLE Courses (
+    id SERIAL PRIMARY KEY,
+    course_name VARCHAR(50) NOT NULL,
+    duration INTERVAL NOT NULL,
+    fees DECIMAL(10, 2) NOT NULL CHECK (fees >0)
+);
+
+--Creating enrollments table
+CREATE TABLE enrollments(
+enrollment_id SERIAL PRIMARY KEY,
+student_id INT NOT NULL,
+course_id INT ,
+enrollment_date DATE DEFAULT CURRENT_DATE,
+
+FOREIGN KEY (student_id) REFERENCES students(id) ,
+FOREIGN KEY (course_id) REFERENCES courses(id)
 )
 
-create table enrollments(
-enrollment_id serial primary key,
-student_id int not null,
-course_id int not null,
-enrollment_date date default current_date,
-
-foreign key (student_id) references students(id) on delete cascade,
-foreign key (course_id) references courses(id) on delete cascade
-)
-
+--Creating enum type gender_enum
 CREATE TYPE gender_enum AS ENUM ('Male', 'Female', 'Other');
 
+--Changing datatype of gender FROM VARCHAR to enum
 ALTER TABLE students
 ALTER COLUMN gender TYPE gender_enum USING gender::gender_enum;
 
+--Populating students table
 INSERT INTO students (id,name,age,gender,email,phone)
 VALUES
 (1, 'Alice Sharma', 20, 'Female', 'alice@gmail.com', '9876543210'),
@@ -44,29 +51,105 @@ VALUES
 (9, 'Kavita Rao', 21, 'Female', 'kavita.rao@gmail.com', '9876500008'),
 (10, 'Ankur Yadav', 20, 'Male', 'ankur.yadav@gmail.com', '9876500009');
 
-INSERT INTO courses VALUES
-(101, 'Web Development', '3 Months', 15000.00),
-(102, 'Data Science', '6 Months', 30000.00),
-(103, 'Graphic Design', '2 Months', 12000.00),
-(104, 'Digital Marketing', '3 Months', 18000.00),
-(105, 'Cybersecurity', '4 Months', 25000.00)
+--Populating courses table
+INSERT INTO courses (id, course_name, duration, fees)
+VALUES 
+    (201, 'mathematics', '6 Months', 60000.00),     
+    (202, 'science', '5 Months', 45000.00),      
+    (203, 'biology', '8 Months', 55000.00),         
+    (204, 'english','7 Months', 50000.00),        
+    (205, 'electronics', '12 years', 58000.00);
 
+--Populating enrollments table
 INSERT INTO enrollments VALUES
-(1, 1, 101, '2025-07-01'),
-(2, 2, 102, '2025-07-02'),
-(3, 3, 103, '2025-07-03'),
-(4, 4, 104, '2025-07-04'),
-(5, 5, 105, '2025-07-05'),
-(6, 6, 101, '2025-07-06'),
-(7, 7, 102, '2025-07-07'),
-(8, 8, 103, '2025-07-08'),
-(9, 9, 104, '2025-07-09'),
-(10, 10, 105, '2025-07-10'),
-(11, 1, 102, '2025-07-11'),
-(12, 2, 103, '2025-07-12'),
-(13, 3, 104, '2025-07-13'),
-(14, 4, 105, '2025-07-14'),
-(15, 5, 101, '2025-07-15');
+(1, 1, 201, '2025-07-01'),
+(2, 2, 202, '2025-07-02'),
+(3, 3, 203, '2025-07-03'),
+(4, 4, 204, '2025-07-04'),
+(5, 5, 205, '2025-07-05'),
+(6, 6, null, '2025-07-06'),
+(7, 7, 201, '2025-07-07'),
+(8, 8, 201, '2025-07-08'),
+(9, 9, 205, '2025-07-09'),
+(10, 10, 205, '2025-07-10'),
+(11, 1, 201, '2025-07-11'),
+(12, 2, null, '2025-07-12'),
+(13, 3, 203, '2025-07-13'),
+(14, 4, 201, '2025-07-14'),
+(15, 5, 205, '2025-07-15');
 
-UPDATE courses set fee=55000.00 where course_name='Cybersecurity'
-select * from courses
+
+SELECT * FROM courses
+
+--List of all students with their enrolled courses.
+SELECT s.name, c.course_name FROM students s LEFT JOIN enrollments e ON s.id=e.student_id
+LEFT JOIN courses c ON e.course_id=c.id
+
+--List all courses along with the number of students enrolled in each.
+SELECT c.course_name, COUNT(e.student_id) FROM courses c LEFT JOIN enrollments e
+ON e.course_id = c.id GROUP BY c.course_name
+
+--List of students who are not enrolled in any course.
+SELECT s.name FROM students s INNER JOIN enrollments e ON s.id=e.student_id WHERE 
+e.course_id IS NULL
+
+--List of students who are enrolled in more than one course.
+SELECT s.name, COUNT(e.student_id) AS courses_count FROM students s INNER JOIN enrollments e ON 
+e.student_id = s.id  GROUP BY s.name HAVING COUNT(e.student_id) >1
+
+--All courses without any enrolled students.
+SELECT c.course_name FROM courses c LEFT JOIN enrollments e ON c.id=e.course_id
+GROUP BY c.course_name
+HAVING COUNT(e.course_id)=0 
+
+ --Details of the student who enrolled most recently
+SELECT s.name,s.age, s.gender, c.course_name FROM students s INNER JOIN enrollments e ON 
+s.id=e.student_id INNER JOIN courses c ON e.course_id=c.id ORDER BY
+enrollment_date DESC LIMIT 1  
+
+--Updating the course fee for "mathematics" to 5500.
+UPDATE courses SET fees=55000.00 WHERE course_name='mathematics'
+
+--Creating enum type status for students table
+CREATE TYPE status AS ENUM ('enrolled', 'dropout');
+
+--Adding column status in students table
+ALTER TABLE Students
+ADD STATUS STATUS NOT NULL DEFAULT 'enrolled';
+
+--Manually dropping out a student
+UPDATE Students
+SET status = 'dropout'
+WHERE id=10;
+
+--Removing all enrollments of a student who has dropped out.
+DELETE FROM enrollments e
+USING students s
+WHERE e.student_id = s.id
+AND s.status = 'dropout';
+
+--Changing the course duration for "electronics" to 18 weeks.
+UPDATE courses SET duration='18 weeks' WHERE id=205  
+
+-- Aggregate functions
+SELECT COUNT(*) FROM students GROUP BY gender HAVING gender='Male';
+SELECT MIN(age) FROM students GROUP BY gender HAVING gender='Female' 
+SELECT MAX(age) FROM students GROUP BY gender HAVING gender='Female' 
+SELECT MAX(fee) FROM courses GROUP BY gender HAVING gander='Male';
+SELECT SUM(age) FROM students GROUP BY gender HAVING gender ='Male' 
+SELECT AVG(age) FROM students GROUP BY gender HAVING gender ='Male' 
+
+--Adding a unique constraINT to the Phone column of the Students table.
+ALTER TABLE students
+ADD UNIQUE (phone);
+
+--Adding an index on the EnrollmentDate column in the Enrollments table.
+CREATE INDEX idx_enrollment_date
+ON enrollments (enrollment_date);
+
+--Ensuring that a course fee cannot be less than 1000.
+ALTER TABLE Courses
+ADD CONSTRAINT CHECK (fees >= 1000);
+
+
+
